4074dc2e2b9bf43b7e6ad48e2df91dcdb2be7777
Sadlark1993 <claudio.zi.junior@gmail.com>
2023-08-04 00:18:48 -0400

    Lesson: Updating Grades Based on Name or ID

--- 02grade-submission/src/main/java/com/ltp/gradesubmission/Grade.java
+++ 02grade-submission/src/main/java/com/ltp/gradesubmission/Grade.java
@@ -1,22 +1,28 @@
 package com.ltp.gradesubmission;
 
+import java.util.UUID;
+
 public class Grade {
   private String name;
   private String subject;
   private String score;
+  private String id;
 
   /*
    * A POJO (Plain Old Java Object) class is the simplest type of Java class;
    * It generates POJO objects;
    * With it, you can create data.
    */
+
   public Grade(String name, String subject, String score) {
     this.name = name;
     this.subject = subject;
     this.score = score;
+    this.id = UUID.randomUUID().toString();
   }
 
   public Grade() {
+    this.id = UUID.randomUUID().toString();
   }
 
   public String getName() {
@@ -41,6 +47,14 @@ public class Grade {
 
   public void setScore(String score) {
     this.score = score;
+  }
+
+  public String getId() {
+    return this.id;
+  }
+
+  public void setId(String id) {
+    this.id = id;
   }
 
 }
--- 02grade-submission/src/main/java/com/ltp/gradesubmission/GradeController.java
+++ 02grade-submission/src/main/java/com/ltp/gradesubmission/GradeController.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class GradeController {
@@ -26,17 +27,27 @@ public class GradeController {
     this.studentGrades.add(new Grade("Neville", "Herbology", "A+"));
   }
 
+  /*
+   * the annotation @RequestParam means that the argument must come from a GET
+   * request, only.
+   */
   @GetMapping("/")
-  public String getForm(Model model) {
-
+  public String getForm(Model model, @RequestParam(required = false) String id) {
+    int index = getGradeIndex(id);
+    // System.out.println(name);
     // created a new POJO object that will be linked to the form
-    model.addAttribute("grade", new Grade());
+    model.addAttribute("grade", index == -1000 ? new Grade() : studentGrades.get(index));
     return "form";
   }
 
   @PostMapping("/handleSubmit")
   public String submitGrade(Grade grade) {
-    studentGrades.add(grade);
+    int index = getGradeIndex(grade.getId());
+    if (index == -1000) { // Registers a new grade
+      studentGrades.add(grade);
+    } else { // Updates existing grade
+      studentGrades.set(index, grade);
+    }
     /*
      * the redirection executes the handle method of /grades. If we returned /grades
      * directly, it would not execute its handler method.
@@ -46,9 +57,16 @@ public class GradeController {
 
   @GetMapping("/grades")
   public String getGrades(Model model) {
-
     model.addAttribute("grades", studentGrades);
     return "grades";
+  }
+
+  public Integer getGradeIndex(String id) {
+    for (int i = 0; i < studentGrades.size(); i++) {
+      if (studentGrades.get(i).getId().equals(id))
+        return i;
+    }
+    return -1000;
   }
 
 }
