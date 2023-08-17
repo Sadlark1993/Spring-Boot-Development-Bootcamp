a9857e3c3460dd1f2b27c9cc3c413c34f5c6b04e
Sadlark1993 <claudio.zi.junior@gmail.com>
2023-08-17 00:51:48 -0400

    Bean annotation and challange

--- 06testing/grade-submission/src/main/java/com/ltp/gradesubmission/repository/GradeRepository.java
+++ 06testing/grade-submission/src/main/java/com/ltp/gradesubmission/repository/GradeRepository.java
@@ -3,12 +3,13 @@ package com.ltp.gradesubmission.repository;
 import java.util.ArrayList;
 import java.util.List;
 
-//import org.springframework.stereotype.Repository;
+import com.ltp.gradesubmission.pojo.Grade;
 
-import com.ltp.gradesubmission.Grade;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public class GradeRepository {
-
+    
     private List<Grade> studentGrades = new ArrayList<>();
 
     public Grade getGrade(int index) {
@@ -22,7 +23,7 @@ public class GradeRepository {
     public void updateGrade(Grade grade, int index) {
         studentGrades.set(index, grade);
     }
-
+    
     public List<Grade> getGrades() {
         return studentGrades;
     }
