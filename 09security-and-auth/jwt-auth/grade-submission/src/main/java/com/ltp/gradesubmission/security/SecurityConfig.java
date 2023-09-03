dca03650b1c80d651063ef6bf4097a1e22e47590
Sadlark1993 <claudio.zi.junior@gmail.com>
2023-09-03 19:50:18 -0400

    JWT Authentication: Authenticate

--- 09security-and-auth/jwt-auth/grade-submission/src/main/java/com/ltp/gradesubmission/security/SecurityConfig.java
+++ 09security-and-auth/jwt-auth/grade-submission/src/main/java/com/ltp/gradesubmission/security/SecurityConfig.java
@@ -8,29 +8,32 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 
+import com.ltp.gradesubmission.security.filter.AuthenticationFilter;
+
 @Configuration
 @AllArgsConstructor
 public class SecurityConfig {
 
   @Bean
   public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+    AuthenticationFilter authenticationFilter = new AuthenticationFilter();
+    authenticationFilter.setFilterProcessesUrl("/authenticate");
     http
-      .headers()
-      .frameOptions()
-      .disable() // New Line: the h2 console runs on a "frame". By default, Spring Security prevents rendering within an iframe. This line disables its prevention.
-      .and()
-      .csrf()
-      .disable()
-      .authorizeRequests()
-      .antMatchers("/h2/**")
-      .permitAll() // New Line: allows us to access the h2 console without the need to authenticate. ' ** '  instead of ' * ' because multiple path levels will follow /h2.
-      .antMatchers(SecurityConstants.REGISTER_PATH)
-      .permitAll()
-      .anyRequest()
-      .authenticated()
-      .and()
-      .sessionManagement()
-      .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+        .headers().frameOptions().disable().and().csrf().disable().authorizeRequests() // New Line: the h2 console runs
+                                                                                       // on a "frame". By default,
+                                                                                       // Spring Security prevents
+                                                                                       // rendering within an iframe.
+                                                                                       // This line disables its
+                                                                                       // prevention.
+
+        .antMatchers("/h2/**").permitAll() // New Line: allows us to access the h2 console without the need to
+                                           // authenticate. ' ** ' instead of ' * ' because multiple path levels will
+                                           // follow /h2.
+        .antMatchers(SecurityConstants.REGISTER_PATH).permitAll()
+        .anyRequest().authenticated()
+        .and()
+        .addFilter(authenticationFilter)
+        .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
     return http.build();
   }
 }
